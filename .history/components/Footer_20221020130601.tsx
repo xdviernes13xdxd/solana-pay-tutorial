ee13530d77563401beb6507083d09f40a913c813
xdviernes13xdxd <carnivorousgang@gmail.com>
2022-10-20 14:07:55 +0200

    lol

--- .history/components/Footer_20221020130601.tsx
+++ .history/components/Footer_20221020130601.tsx
@@ -2,12 +2,12 @@ export default function Footer() {
   return (
     <footer className="flex w-full justify-center border-t-2 border-gray-900 py-12 h-10">
       <a
-        href='https://www.twitter.com/wild3_eth/'
+        href='https://www.pointer.gg?utm_source=solana-pay'
         target='_blank'
         rel='noopener noreferrer'
       >
-        Built by{" "}
-        <span>Wild3 Labs</span>
+        Learn web3 dev and earn crypto rewards at{" "}
+        <span>Pointer</span>
       </a>
     </footer>
   )
